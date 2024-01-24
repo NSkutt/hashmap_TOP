c5569e1ec02d7aa77fef3e933ef309c56b9b760c
N Skutt <nskutt@protonmail.com>
2024-01-24 16:12:05 -0500

    rewritesthe already existing functions due to a better understanding of what was needed, fills in placeholder functions up to and including the clear function

--- hashmap.rb
+++ hashmap.rb
@@ -1,49 +1,68 @@
 # frozen_string_literal: true
 
 # This line will be used when buckets are accesed by index
-# raise IndexError if index.negative? || index >= @buckets.length
 
 # TOP Project HashMap
 class HashMap
   def initialize
     @buckets = Array.new(16)
     @load_factor = 0.75
-    @old_length = @buckets.length
   end
 
-  def grow
-    @buckets += Array.new(@buckets.length) if hash.length >= @load_factor * @buckets.length
+  def growth_check
+    if hash.length >= @load_factor * @buckets.length
+      @buckets += Array.new(@buckets.length)
+
+    end
+  end
+
+  def error_check(index)
+    raise IndexError if index.negative? || index >= @buckets.length
   end
 
   def hash(str)
     hash_code = 0
-    string.each_char { |char| hash_code += char.ord }
-    set(hash_code, str)
+    prime_num = 31
+    str.each_char { |char| prime_num * hash_code += char.ord }
+    hash_code % @buckets.length
+    error_check(hash_code)
+    hash_code
   end
 
   def set(k, val)
-    place = k % @buckets.length
-    @buckets[place] = [val, @buckets[place]]
+    idx = hash(k)
+
+    @buckets[idx] = [k, val, @buckets[idx]]
+    growth_check
   end
 
-  def get
-
+  def get(key)
+    idx = hash(key)
+    error_check(idx)
+    @buckets[idx][1]
   end
 
-  def key?
-
+  def key?(key)
+    idx = hash(key)
+    error_check(idx)
+    @buckets[idx].flatten.include?(key)
   end
 
-  def remove
-
+  def remove(key)
+    idx = hash(key)
+    error_check(idx)
+    key?(key) ? @buckets.delete_at(idx) : nil
   end
 
   def length
-
+    filled_buckets = 0
+    @buckets.for_each { |bucket| bucket.nil? ? next : filled_buckets += 1 }
+    filled_buckets
   end
 
   def clear
-
+    @buckets.each { |bucket| bucket = nil}
+    @buckets = Array.new(16)
   end
 
   def keys
