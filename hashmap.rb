8c4a0b6702fb5666fe545d595234ef7364d51bc1
N Skutt <nskutt@protonmail.com>
2024-02-09 17:29:11 -0500

    Creates a working (but not bug free) remove method,  adds error checks for the various retrieval methods, adds a way to directly interact with the HashMap class and use its methods by running the rb file.

--- hashmap.rb
+++ hashmap.rb
@@ -30,16 +30,22 @@ class HashMap
   def hash(str)
     hash_code = 0
     prime_num = 31
-    str.each_char { |char| prime_num * hash_code += char.ord }
+    str.each_char { |char| prime_num * hash_code += char.ord - 1 }
     index = hash_code % @buckets.length
     raise IndexError if index.negative? || index >= @buckets.length
 
     index
   end
 
+  def compactor(idx)
+    @buckets[idx].compact! if @buckets[idx].is_a?(Array)
+    @buckets.each { |item| item.is_a?(Array) ? item.compact! : next }
+  end
+
   def set(key, val)
     idx = checkout(key)
     @buckets[idx] = [key, val, @buckets[idx]]
+    compactor(idx)
     growth_check
   end
 
@@ -56,10 +62,24 @@ class HashMap
   end
 
   def remove(key)
-    idx = checkout(key)
-    nil unless key?(key)
+    bucket = @buckets[hash(key)]
+    return nil unless bucket.is_a?(Array)
 
+    holding_array = Marshal.load(Marshal.dump(bucket))
+    clean_array = exterminate(key, bucket)
+    subtracted_items = holding_array - clean_array
+    subtracted_items.flatten!(1) if subtracted_items.first.is_a?(Array)
+    subtracted_items.reject { |item| item.is_a?(Array) }
+  end
 
+  def exterminate(key, bucket)
+    bucket.each { |item| exterminate(key, item) if item.is_a?(Array) }
+    bucket.reject!(&:empty?)
+    return bucket unless bucket.first == key
+
+    bucket.select! { |item| item.is_a?(Array) }
+    bucket.flatten!(1)
+    bucket
   end
 
   def length
@@ -74,7 +94,7 @@ class HashMap
   end
 
   def keys(arr = @buckets, keychain = [])
-    keychain.push(arr.first) unless arr.first.nil?
+    keychain.push(arr.first) unless arr.first.nil? || arr.first.is_a?(Array)
     arr.each do |bucket|
       keys(bucket, keychain) if bucket.is_a?(Array)
     end
@@ -82,7 +102,7 @@ class HashMap
   end
 
   def values(arr = @buckets, lockbox = [])
-    lockbox.push(arr[1]) unless arr.first.nil?
+    lockbox.push(arr[1]) unless arr.first.nil? || arr.first.is_a?(Array)
     arr.each do |bucket|
       values(bucket, lockbox) if bucket.is_a?(Array)
     end
@@ -90,10 +110,27 @@ class HashMap
   end
 
   def entries(arr = @buckets, log = [])
-    log.push([arr.first, arr[1]]) unless arr.first.nil?
+    log.push([arr.first, arr[1]]) unless arr.first.nil? || arr.first.is_a?(Array)
     arr.each do |bucket|
       entries(bucket, log) if bucket.is_a?(Array)
     end
     log
   end
+end
+
+hmp = HashMap.new
+loop do
+  p 'Method name or EXIT'
+  com = gets.chomp!
+  break if com == 'EXIT'
+  next unless hmp.respond_to?(com)
+
+  p 'Applicable arguments'
+  args = []
+  p 'Argument 1'
+  args.push(gets.chomp!)
+  p 'Argument 2'
+  args.push(gets.chomp!)
+  args.delete_if(&:empty?)
+  print "#{hmp.__send__(com, *args)}\n"
 end
